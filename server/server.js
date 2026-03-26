fa0c7a09c81747aca3fdea915988663786339eec
Tusharkhadde <tusharkhadde06@gmail.com>
2026-03-26 11:50:21 +0530

    Added backend api

--- server/server.js
+++ server/server.js
@@ -47,7 +47,9 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+// Static files
 app.use('/uploads', express.static(uploadsDir));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Request logging
 app.use((req, res, next) => {
@@ -1686,9 +1688,13 @@ app.use((err, req, res, next) => {
   });
 });
 
-// 404 handler
-app.use((req, res) => {
-  res.status(404).json({ success: false, message: `Route ${req.originalUrl} not found` });
+// Catch-all route to serve the frontend for any non-API request
+app.get('*', (req, res) => {
+  if (!req.path.startsWith('/api')) {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  } else {
+    res.status(404).json({ success: false, message: `Route ${req.originalUrl} not found` });
+  }
 });
 
 // ==================== START SERVER ====================
