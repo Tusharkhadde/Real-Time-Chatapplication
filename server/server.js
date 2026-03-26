52f1c407bf8161d96c76b5c4d1483d406762ed5e
Tusharkhadde <tusharkhadde06@gmail.com>
2026-03-26 11:58:29 +0530

    Fixed Server file

--- server/server.js
+++ server/server.js
@@ -47,10 +47,6 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// Static files
-app.use('/uploads', express.static(uploadsDir));
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
 // Request logging
 app.use((req, res, next) => {
   console.log(`📨 ${req.method} ${req.path}`);
@@ -1688,7 +1684,11 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Catch-all route to serve the frontend for any non-API request
+// Static files
+app.use('/uploads', express.static(uploadsDir));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Catch-all route to serve the frontend for any non-API request (must be after all other routes)
 app.get('*', (req, res) => {
   if (!req.path.startsWith('/api')) {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
